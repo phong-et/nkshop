fb8cdbeb3a876068aa574202741ba4780efdc161
Phillip <phillip7.et@gmail.com>
2019-07-16 23:20:03 +0700

    Load product all pages (input limitted page number & start page)
    400 pages tested

--- nk.js
+++ nk.js
@@ -55,6 +55,26 @@ function writeProduct(productId, content) {
     })
   })
 }
+// folder = SGBP/
+function writeProductsCity(fileName, folder, content) {
+  return new Promise((resolve, reject) => {
+    let dir = DIR_PRODUCTS + folder
+    fileName = dir + '/' + fileName + '.json'
+
+    if (!fs.existsSync(DIR_PRODUCTS)) {
+      fs.mkdirSync(DIR_PRODUCTS)
+    }
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir)
+    }
+    fs.writeFile(fileName, content, function (err) {
+      if (err) reject(err)
+      var statusText = 'write file > ' + fileName + ' success'
+      //log(statusText)
+      resolve(statusText)
+    })
+  })
+}
 function writeReviews(productId, content) {
   return new Promise((resolve, reject) => {
     let dir = DIR_PRODUCTS + productId + '/' + DIR_REVIEWS,
@@ -123,7 +143,7 @@ async function fetchImagesOfReview(nkJson, productId) {
       let dir = DIR_PRODUCTS + productId + '/' + DIR_REVIEWS
       let url = e.data.dimensions.original.url
       let encodeUrl = url.substring(0, url.lastIndexOf('review-of-') + 1) + encodeURIComponent(url.substring(url.lastIndexOf('review-of-') + 1, url.length))
-      log(encodeUrl);
+      //log(encodeUrl);
       let fileName = dir + url.substring(url.lastIndexOf('/') + 1)
       downloadImage(fileName, encodeUrl, () => { })
     })
@@ -227,4 +247,103 @@ async function fetchProducts(url, productIds) {
   } catch (error) {
     log(error.message)
   }
+}
+
+var isUsedCookie = false,
+  jar
+async function fetchProductsByCity(productUrl, cityCode, minPrice, minReviewCount) {
+  try {
+    var qs = {
+      cityCode: encodeURI(cityCode),
+      mode: 'directory',
+      offset: '0',
+      orderBy: 'byPriceDesc'
+    }
+    var options = {
+      url: productUrl,
+      headers: headers,
+      qs: qs
+    }
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        var json = JSON.parse(body);
+        log(json)
+      }
+      else {
+        log(response.headers)
+        log(response.statusCode)
+        if (!isUsedCookie) {
+          jar = request.jar()
+          var arrayCookie = ['__cfduid=dfea12d33ef91eeba002fbdec124e2cc01561876504; cf_clearance=19ae0c872ccde7e6768229e5fb65261b126fa86a-1561876508-604800-150; 06cb36fc0e9a783099b7974a96a6d0c0=40euic29k024onc4u8qb8f4n31; base_language_id=2; __utma=159633447.272160313.1561876512.1561876512.1561876512.1; __utmc=159633447; __utmz=159633447.1561876512.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)']
+          arrayCookie.forEach((cookies) => {
+            cookies.split(';').forEach((cookie) => {
+              jar.setCookie(request.cookie(cookie.trim()), productUrl)
+            })
+          })
+          isUsedCookie = true;
+          options['jar'] = jar
+          log(options)
+          request(options, (e, r, body) => {
+            log(r.headers)
+            log(r.statusCode)
+            log(body)
+          })
+        }
+      }
+    });
+  } catch (error) {
+    log(error.message)
+  }
+}
+
+// Recursive load all pages
+function fetchProductsSGByPriceDescOnePage(page, callback) {
+  let offset = page == 1 ? 0 : page * 20
+  log(`offset = ${offset} || page = ${page}`);
+  request(cfg.productUrl + '?cityCode=' + cfg.cities[0] + '&mode=directory&offset=' + offset + '&orderBy=byPriceDesc', function (error, response, body) {
+    //log('error:', error);
+    //log('statusCode:', response && response.statusCode);
+    //log('headers:', response && response.headers);
+    //console.log('body:', body);
+    callback(body);
+  })
+}
+// Test OnePage
+// fetchProductsSGByPriceDescOnePage(0, (data) => {
+//   writeProductsCity('P1', 'SGBPD/', data)
+// })
+
+let page = 2,
+  nkProducts = []
+function fetchProductsSGByPriceDescAllPage(i) {
+  try {
+    fetchProductsSGByPriceDescOnePage(i, (data) => {
+      //log(data);
+      nkProducts = nkProducts.concat(JSON.parse(data))
+      writeProductsCity('P' + i, 'SGBPD/', data)
+      i = i + 1
+      if (i <= page) {
+        setTimeout(() => {
+          fetchProductsSGByPriceDescAllPage(i)
+        }, 1000)
+      }
+      else {
+        log('ProductIds is leaded')
+        log(nkProducts.length);
+        var nkProductIds = nkProducts.map(product => product.id)
+        log(nkProductIds);
+        fetchProducts(cfg.productUrl, nkProductIds)
+        writeProductsCity('P_ALL', 'SGBPD/', JSON.stringify(nkProducts))
+      }
+    })
+  } catch (error) {
+    log('fetchProductsSGByPriceDescAllPage')
+    log(error)
+  }
+}
+fetchProductsSGByPriceDescAllPage(2)
+
+
+module.exports = {
+  downloadImage: downloadImage
 }
