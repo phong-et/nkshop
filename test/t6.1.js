974cc800ce1d695a23f54eb24514e8c944fbb4e1
Phillip <phillip7.et@gmail.com>
2019-09-19 05:03:07 +0700

    apply best practice mongoose connection db (price and rating count num)
    try change price and rating count of product model to number

---
+++ test/t6.1.js
@@ -0,0 +1,62 @@
+var cfg = require('../nk.cfg.js')
+const mongoose = require('mongoose');
+const db = require('../api/db')
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+const productSchema = new Schema({
+    _id: ObjectId,
+    id: String,
+    name: String,
+    price: Number,
+    locationId: String,
+    status: String,
+    expirationDate: Date,
+    userId: String,
+    lastUpdateStamp: String,
+    meta: String,
+    lat: String,
+    lng: String,
+    districtId: String,
+    cityId: String,
+    cover: {
+        baseName: String,
+        dimensions: {
+            small: {
+                file: String,
+                width: Number,
+                height: Number,
+                url: String
+            },
+            original: {
+                file: String,
+                width: Number,
+                height: Number,
+                url: String
+            },
+            thumbnail: {
+                file: String,
+                width: Number,
+                height: Number,
+                url: String
+            }
+        }
+    },
+    ratingScore: String,
+    ratingCount: Number,
+    ratingAvg: String,
+    photoCount: String,
+    timestamp: String,
+    viewCount: String,
+    address: String,
+    slug: String
+});
+const productModel = db.model('product', productSchema);
+productModel.find({
+    $and: [
+        { '$where': 'this.price >= 1000' },
+        { ratingCount: "5" }
+    ]
+}, 'id name price', (err, data) => {
+    console.log(data);
+    db.connection.close();
+});
