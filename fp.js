4d9da1e0101f307c34ef744509dc476f13c3a81c
Phillip <phillip7.et@gmail.com>
2019-10-12 23:57:22 +0700

    ...

--- fp.js
+++ fp.js
@@ -1,17 +1,32 @@
 var nk = require('./nk');
 var cfg = require('./nk.cfg')
-nk.fetchProducts(cfg.productUrl,
-  [
-    
-  ])
+// nk.fetchProducts(cfg.productUrl,
+//   [
+//     24855
+//   ])
+
+nk.fetchProductDetailByListId(cfg.productUrl, [
+  25736,
+  // 23229,
+  // 25409
+])
 //nk.fetchProductsSGByPriceDescAllPage(1,15);
-nk.fetchProductsByIdRange(
-  cfg.productUrl, 
-  0, 
-  //25147,
-  //25728, 
-  { price: 100, ratingCount: 10 }
-);
+
+// nk.fetchProductsByIdRange(
+//   cfg.productUrl, 
+//   23000,
+//   24000, 
+//   //25146,
+//   //25728, 
+//   { price: 500, ratingCount: 3 }
+// )
+
+// nk.fetchProductsByIdRange(
+//   cfg.productUrl, 
+//   25817,
+//   24000,
+// )
+
 //nk.fetchJsonOfProduct(cfg.productUrl, id)
 
 /**
@@ -21,4 +36,7 @@ nk.fetchProductsByIdRange(
 Error: connect ETIMEDOUT ip
     at TCPConnectWrap.afterConnect [as oncomplete] (net.js:1097:14)
 Error: connect ETIMEDOUT ip
-    */
+    
+Error: read ECONNRESET
+downloadImage.request.head:Error: connect ETIMEDOUT 104.25.90.6:443
+*/
