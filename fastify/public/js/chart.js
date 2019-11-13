e826fa6ca7dc06be155882119d38a17b95d12dbe
Phillip <phillip7.et@gmail.com>
2019-11-13 22:37:25 +0700

    fix chart product and add resize chart tool

--- fastify/public/js/chart.js
+++ fastify/public/js/chart.js
@@ -1,15 +1,15 @@
-// Create the chart
-function drawChart(data, title, subTitle, titleX, titleY) {
-    Highcharts.chart('container', {
+let chart = opener.window.chart,
+    data = chart.data,
+    cfg = chart.cfg,
+    Chart = Highcharts.chart('container', {
         chart: {
             type: 'column',
-            //width:1700
         },
         title: {
-            text: title
+            text: cfg.title,
         },
         subtitle: {
-            text: subTitle
+            text: cfg.subTitle
         },
         accessibility: {
             announceNewData: {
@@ -19,12 +19,12 @@ function drawChart(data, title, subTitle, titleX, titleY) {
         xAxis: {
             type: 'category',
             title: {
-                text: titleX
+                text: cfg.titleX
             }
         },
         yAxis: {
             title: {
-                text: titleY
+                text: cfg.titleY
             }
         },
         legend: {
@@ -48,47 +48,6 @@ function drawChart(data, title, subTitle, titleX, titleY) {
                 name: "Price",
                 colorByPoint: true,
                 data: data
-                // [
-                //     {
-                //         name: "550K",
-                //         y: 100,
-                //     },
-                //     {
-                //         name: "Firefox",
-                //         y: 10.57,
-                //     },
-                //     {
-                //         name: "Internet Explorer",
-                //         y: 7.23,
-                //     },
-                //     {
-                //         name: "Safari",
-                //         y: 5.58,
-                //     },
-                //     {
-                //         name: "Edge",
-                //         y: 4.02,
-                //     },
-                //     {
-                //         name: "Opera",
-                //         y: 1.92,
-                //     },
-                //     {
-                //         name: "Other",
-                //         y: 7.62,
-                //     }
-                // ]
             }
         ]
     })
-}
-let chart = opener.window['chart'],
-    data = chart.data,
-    cfg = chart.cfg
-drawChart(
-    data,
-    cfg.title,
-    cfg.subTitle,
-    cfg.titleX,
-    cfg.titleY
-)
