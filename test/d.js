7cfaeba6018a5674cee8fe6cdce7397c19049440
Phillip <phillip7.et@gmail.com>
2019-11-16 19:48:15 +0700

    add test date output of all toString() of date

---
+++ test/d.js
@@ -0,0 +1,12 @@
+let d = new Date()
+console.log(`
+    d.toLocaleDateString() => ${d.toLocaleDateString()}
+    d.toLocaleTimeString() => ${d.toLocaleTimeString()}
+    d.toLocaleString() => ${d.toLocaleString()}
+    d.toDateString() => ${d.toDateString()}
+    d.toTimeString() => ${d.toTimeString()}
+    d.toISOString() => ${d.toISOString()}
+    d.toUTCString() => ${d.toUTCString()}
+    d.toString() => ${d.toString()}
+    d.toJSON() => ${d.toJSON()}
+`)
