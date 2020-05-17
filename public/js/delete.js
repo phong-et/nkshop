f5fa15ae53de289be5393ccb3d003ed92b60bb9d
Phillip <phillip7.et@gmail.com>
2020-05-17 21:02:27 +0700

    disabled deleting all products

--- public/js/delete.js
+++ public/js/delete.js
@@ -18,7 +18,7 @@ function deleteProduct(productId, btnDelete) {
                 try {
                     if (res.success) {
                         alert('deleted')
-                        $(btnDelete).parent().parent().parent().detach()
+                        $(btnDelete).parent().parent().detach()
                     }
                     else
                         log(res.responseText.msg)
@@ -32,40 +32,39 @@ function deleteProduct(productId, btnDelete) {
         });
     }
 }
-
-function deleteProducts(index, limitIndex) {
-    // use recursive native
-    let productId = globalProducts[index].id
-    let btnDelete = document.getElementsByClassName('btnDelete')[index];
-    $(btnDelete).parent().parent().parent().addClass('active')
-    if ($('#cbFocusProductItem').is(':checked')) $(btnDelete).focus()
-    let spiner = $(btnDelete).parent().prev()
-    spiner.prop('class', 'fas fa-sync fa-spin')
-    $.ajax({
-        url: '/products/delete/' + productId,
-        type: 'GET',
-        data: {
-            isDeleteAtDatabase: $('#cbDeleteProductAtDatabase').is(':checked')
-        },
-        success: function (res) {
-            try {
-                if (res.success) {
-                    $(btnDelete).parent().parent().parent().detach()
-                }
-                console.log(res)
-                index++
-                if (index < limitIndex)
-                    deleteProducts(index, limitIndex)
-                else {
-                    log('Done Delete All Products')
-                    drawProduct(globalReviewedProduct)
-                }
-            } catch (error) {
-                log(error)
-            }
-        },
-        error: function (err) {
-            log(err)
-        }
-    });
-}
+// function deleteProducts(index, limitIndex) {
+//     // use recursive native
+//     let productId = globalProducts[index].id
+//     let btnDelete = document.getElementsByClassName('btnDelete')[index];
+//     $(btnDelete).parent().parent().parent().addClass('active')
+//     if ($('#cbFocusProductItem').is(':checked')) $(btnDelete).focus()
+//     let spiner = $(btnDelete).parent().prev()
+//     spiner.prop('class', 'fas fa-sync fa-spin')
+//     $.ajax({
+//         url: '/products/delete/' + productId,
+//         type: 'GET',
+//         data: {
+//             isDeleteAtDatabase: $('#cbDeleteProductAtDatabase').is(':checked')
+//         },
+//         success: function (res) {
+//             try {
+//                 if (res.success) {
+//                     $(btnDelete).parent().parent().parent().detach()
+//                 }
+//                 console.log(res)
+//                 index++
+//                 if (index < limitIndex)
+//                     deleteProducts(index, limitIndex)
+//                 else {
+//                     log('Done Delete All Products')
+//                     drawProduct(globalReviewedProduct)
+//                 }
+//             } catch (error) {
+//                 log(error)
+//             }
+//         },
+//         error: function (err) {
+//             log(err)
+//         }
+//     });
+// }
